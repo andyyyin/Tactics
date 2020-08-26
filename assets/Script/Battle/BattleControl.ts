6b685334459109cdabcc8f90aeb41d35574225d2
andyyyin <andyyyin@outlook.com>
2020-08-26 22:10:34 +0800

    相机移动边界限制

--- assets/Script/Battle/BattleControl.ts
+++ assets/Script/Battle/BattleControl.ts
@@ -1,6 +1,30 @@
 const {ccclass, property} = cc._decorator;
 import {DIRECTION} from "../Global/Enums";
 
+const getNodeAround = (node) => {
+	return {
+		left: (node.x + (0 - node.anchorX) * node.width),
+		right: (node.x + (1 - node.anchorX) * node.width),
+		centerX: (node.x + (0.5 - node.anchorX) * node.width),
+		top: (node.y + (1 - node.anchorY) * node.height),
+		bottom: (node.y + (0 - node.anchorY) * node.height),
+		centerY: (node.y + (0.5 - node.anchorY) * node.height),
+	}
+}
+
+const setNodeAround = (node, p) => {
+	let {left, right, top, bottom, centerX, centerY} = p
+	if (left) { node.x = left - (0 - node.anchorX) * node.width }
+	else if (right) { node.x = right - (1 - node.anchorX) * node.width }
+	if (top) { node.y = top - (1 - node.anchorY) * node.height }
+	else if (bottom) { node.y = bottom - (0 - node.anchorY) * node.height }
+	if (centerX) node.x = centerX - (0.5 - node.anchorX) * node.width
+	if (centerY) node.y = centerY - (0.5 - node.anchorY) * node.width
+}
+
+let _cameraAround
+let _mapAround
+
 @ccclass
 export default class BattleControl extends cc.Component {
 
@@ -16,6 +40,9 @@ export default class BattleControl extends cc.Component {
 	@property(cc.Prefab)
 	ButtonPrefab = null
 
+	@property(cc.Integer)
+	cameraSpeed = 600
+
 	cameraMovingX = 0
 	cameraMovingY = 0
 
@@ -29,7 +56,15 @@ export default class BattleControl extends cc.Component {
 		cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
 		this.MapNode.on(cc.Node.EventType.MOUSE_DOWN, this.onMouseDown, this)
 		this.MapNode.on(cc.Node.EventType.MOUSE_UP, this.onMouseUp, this)
+		_mapAround = getNodeAround(this.MapNode)
+		console.log('map', _mapAround)
+		_cameraAround = getNodeAround(this.CameraNode)
+		if (this.MapNode.width < this.CameraNode.width) {
+			let {centerX} = getNodeAround(this.MapNode)
+			setNodeAround(this.CameraNode, {centerX})
+		}
 	}
+
 
 	protected onDestroy(): void {
 		cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
@@ -104,13 +139,45 @@ export default class BattleControl extends cc.Component {
 
 	updateCamera (dt) {
 		if (this.isShowingPanel) return
-		let speed = 600 // todo 在什么地方提供配置接口
+		let speed = this.cameraSpeed
 		// todo 相机移动添加边缘限制
+
+
+
 		if (this.cameraMovingX) {
-			this.CameraNode.x += (this.cameraMovingX * speed * dt)
+			if (this.cameraMovingX < 0 && _cameraAround.left > _mapAround.left) {
+				this.CameraNode.x += (this.cameraMovingX * speed * dt)
+				let {left} = getNodeAround(this.CameraNode)
+				if (left < _mapAround.left) {
+					setNodeAround(this.CameraNode, {left: _mapAround.left})
+				}
+			}
+			if (this.cameraMovingX > 0 && _cameraAround.right < _mapAround.right) {
+				this.CameraNode.x += (this.cameraMovingX * speed * dt)
+				let {right} = getNodeAround(this.CameraNode)
+				if (right > _mapAround.right) {
+					setNodeAround(this.CameraNode, {right: _mapAround.right})
+				}
+			}
+			_cameraAround = getNodeAround(this.CameraNode)
 		}
 		if (this.cameraMovingY) {
-			this.CameraNode.y += (this.cameraMovingY * speed * dt)
+			if (this.cameraMovingY > 0 && _cameraAround.top < _mapAround.top) {
+				this.CameraNode.y += (this.cameraMovingY * speed * dt)
+				let {top} = getNodeAround(this.CameraNode)
+				if (top > _mapAround.top) {
+					setNodeAround(this.CameraNode, {top: _mapAround.top})
+				}
+			}
+			if (this.cameraMovingY < 0 && _cameraAround.bottom > _mapAround.bottom) {
+				this.CameraNode.y += (this.cameraMovingY * speed * dt)
+				let {bottom} = getNodeAround(this.CameraNode)
+				if (bottom < _mapAround.bottom) {
+					setNodeAround(this.CameraNode, {bottom: _mapAround.bottom})
+				}
+			}
+			_cameraAround = getNodeAround(this.CameraNode)
+			// console.log({top, bottom})
 		}
 	}
 
